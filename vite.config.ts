60e787c624182a479905dd78b5bba03268758b5b
Vladislav Shell <shell-vladislav@mail.ru>
2025-02-23 23:47:31 +0700

    Set up vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@classes': path.resolve(__dirname, './src/classes'),
+    },
+  },
+  plugins: [react(), vercel()],
   server: {
+    host: 'localhost',
     port: 3000,
   },
-})
+});
